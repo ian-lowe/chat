0288d517db9b001eb4514a0153b24a3d59cd0dcf
ian-lowe <ilowe@mac.com>
2019-11-02 19:53:40 -0700

    replaced snake case with camel case, and refactored dom selector into a variable

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,7 @@
 var socket = io();
 
 const leftSidebar = document.querySelector(".div1");
+const messageInput = document.querySelector(".div4 input");
 
 // nickname and currentRoom taken from index.html
 
@@ -12,17 +13,17 @@ socket.on("connect", function () {
     }
 
     socket.emit("join", currentRoom, nickname);
-    document.querySelector(".div4 input").setAttribute("placeholder", "Message " + currentRoom);
+    messageInput.setAttribute("placeholder", "Message " + currentRoom);
 
     socket.emit("get channels");
-    socket.on("receive channels", function (current_channels) {
-        for (let i = 0; i < current_channels.length; i++) {
-            let new_div = document.createElement("div");
-            new_div.innerText = current_channels[i];
-            new_div.addEventListener("click", function() {
-                initChannel(new_div, current_channels[i]);
+    socket.on("receive channels", function (currentChannels) {
+        for (let i = 0; i < currentChannels.length; i++) {
+            let newDiv = document.createElement("div");
+            newDiv.innerText = currentChannels[i];
+            newDiv.addEventListener("click", function() {
+                initChannel(newDiv, currentChannels[i]);
             });
-            leftSidebar.append(new_div);
+            leftSidebar.append(newDiv);
         }
 
         let channels = document.querySelectorAll('.div1 div');
@@ -35,51 +36,49 @@ socket.on("connect", function () {
 
     // functionality for channel creation
     document.querySelectorAll("button")[0].addEventListener("click", function () {
-        let channel_name = document.getElementById("new-channel").value;
-        if (channel_name == "") {
+        let channelName = document.getElementById("new-channel").value;
+        if (channelName == "") {
             return;
         }
-        channel_name = channel_name;
-        channel_name = channel_name.toLowerCase();
+        channelName = channelName;
+        channelName = channelName.toLowerCase();
         document.getElementById("new-channel").value = "";
-        socket.emit("create channel", channel_name);
+        socket.emit("create channel", channelName);
     });
 
     document.querySelector(".div2 input").addEventListener("keyup", function () {
         if (event.key === "Enter") {
-            let channel_name = document.getElementById("new-channel").value;
-            if (channel_name == "") {
+            let channelName = document.getElementById("new-channel").value;
+            if (channelName == "") {
                 return;
             }
-            channel_name = channel_name;
-            channel_name = channel_name.toLowerCase();
+            channelName = channelName;
+            channelName = channelName.toLowerCase();
             document.getElementById("new-channel").value = "";
-            socket.emit("create channel", channel_name);
+            socket.emit("create channel", channelName);
         }
     });
 
     // functionality for sending a message
     document.querySelectorAll("button")[1].addEventListener("click", function () {
-        let message_input = document.querySelector(".div4 input");
-        let message = message_input.value;
+        let message = messageInput.value;
         if (message == "") {
             return;
         }
-        message_input.value = "";
+        messageInput.value = "";
         if (window.innerWidth > 499) {
-            message_input.focus();
+            messageInput.focus();
         }
         socket.emit("submit msg", message, nickname, currentRoom);
     });
 
-    document.querySelector(".div4 input").addEventListener("keyup", function () {
+    messageInput.addEventListener("keyup", function () {
         if (event.key === "Enter") {
-            let message_input = document.querySelector(".div4 input");
-            let message = message_input.value;
+            let message = messageInput.value;
             if (message == "") {
                 return;
             }
-            message_input.value = "";
+            messageInput.value = "";
             socket.emit("submit msg", message, nickname, currentRoom);
         }
     });
@@ -88,33 +87,33 @@ socket.on("connect", function () {
 });
 
 // set up any newly created channel to accept click event
-socket.on("broadcast channel", function (channel_name) {
+socket.on("broadcast channel", function (channelName) {
     // create div for channel
-    let new_div = document.createElement("div");
-    new_div.innerText = channel_name;
-    leftSidebar.append(new_div);
+    let newDiv = document.createElement("div");
+    newDiv.innerText = channelName;
+    leftSidebar.append(newDiv);
     // set up newly created channel to accept click event
-    new_div.addEventListener("click", function () {
-        initChannel(new_div, channel_name);
+    newDiv.addEventListener("click", function () {
+        initChannel(newDiv, channelName);
     });
 });
 
 socket.on("render room", function (messages) {
-    let message_list = document.querySelector("#messages");
-    while (message_list.hasChildNodes()) {
-        message_list.removeChild(message_list.lastChild);
+    let messageList = document.querySelector("#messages");
+    while (messageList.hasChildNodes()) {
+        messageList.removeChild(messageList.lastChild);
     }
     for (msg of messages) {
-        const name_time = document.createElement("li");
+        const nameTime = document.createElement("li");
         const text = document.createElement("li");
-        var local_time = new Date(msg["timestamp"]);
-        local_time_parsed = local_time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-        name_time.innerHTML = `<span>${msg["nickname"]}</span> ${local_time_parsed}:`;
+        var localTime = new Date(msg["timestamp"]);
+        localTimeParsed = localTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+        nameTime.innerHTML = `<span>${msg["nickname"]}</span> ${localTimeParsed}:`;
         text.innerText = `${msg["message"]}`;
-        name_time.classList.add("name-time");
+        nameTime.classList.add("name-time");
         text.classList.add("text");
 
-        document.querySelector("#messages").append(name_time);
+        document.querySelector("#messages").append(nameTime);
         document.querySelector("#messages").append(text);
     }
     let messageBody = document.querySelector('.div3');
@@ -122,22 +121,22 @@ socket.on("render room", function (messages) {
 });
 
 socket.on("broadcast msg", function (message, nickname, timestamp) {
-    var local_time = new Date(timestamp);
-    local_time_parsed = local_time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    var localTime = new Date(timestamp);
+    localTimeParsed = localTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
     var messageBody = document.querySelector('.div3');
-    const name_time = document.createElement("li");
+    const nameTime = document.createElement("li");
     const text = document.createElement("li");
-    name_time.innerHTML = `<span>${nickname}</span> ${local_time_parsed}:`;
+    nameTime.innerHTML = `<span>${nickname}</span> ${localTimeParsed}:`;
     text.innerText = `${message}`;
-    name_time.classList.add("name-time");
+    nameTime.classList.add("name-time");
     text.classList.add("text");
 
-    document.querySelector("#messages").append(name_time);
+    document.querySelector("#messages").append(nameTime);
     document.querySelector("#messages").append(text);
     var messageBody = document.querySelector('.div3');
     // keep chat scrolled to the bottom unless the user is scrolled up to view previous messages
-    let message_height = messageBody.scrollTop + text.offsetHeight + name_time.offsetHeight;
-    if (message_height >= (messageBody.scrollHeight - messageBody.clientHeight)) {
+    let messageHeight = messageBody.scrollTop + text.offsetHeight + nameTime.offsetHeight;
+    if (messageHeight >= (messageBody.scrollHeight - messageBody.clientHeight)) {
         messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
     }
 });
@@ -160,12 +159,12 @@ socket.on("disconnected", function (nickname) {
     messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
 });
 
-const initChannel = function(self, channel_name) {
+const initChannel = function(self, channelName) {
     if (self.innerText !== currentRoom) {
         // update page title to reflect new channel
         document.title = "Flack " + self.innerText;
         // set message placeholder to reflect current channel
-        document.querySelector(".div4 input").setAttribute("placeholder", "Message " + self.innerText);
+        messageInput.setAttribute("placeholder", "Message " + self.innerText);
         // style channels to reflect selection
         let channels = document.querySelectorAll(".div1 div");
         for (channel of channels) {
@@ -175,11 +174,11 @@ const initChannel = function(self, channel_name) {
         // on larger browsers, keep message box in focus regardless of channel switch
         // needed because on phones, focusing of the input brings up the keyboard
         if (window.innerWidth > 499) {
-            document.querySelector(".div4 input").focus();
+            messageInput.focus();
         }
         // join room
         socket.emit("leave", currentRoom);
-        currentRoom = channel_name;
+        currentRoom = channelName;
         socket.emit("join", currentRoom, nickname);
     }
 }
